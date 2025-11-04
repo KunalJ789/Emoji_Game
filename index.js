ff777478966ae11067d23116e6a8b2088d378977
KunalJ789 <aizen6730@gmail.com>
2025-11-04 17:27:31 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+let fighters = [
+  "🫎",
+  "🫏",
+  "🦁",
+  "🐱",
+  "🐯",
+  "🦒",
+  "🦊",
+  "🦝",
+  "🐰",
+  "🐹",
+  "🐭",
+  "🐗",
+  "🐷",
+  "🐮",
+  "🐻",
+  "🐻‍❄️",
+  "🐨",
+  "🐼",
+  "🐸",
+  "🦓",
+  "🐲",
+];
+let stageEl = document.getElementById("stage");
+let fightButton = document.getElementById("fightBtn");
+
+fightButton.addEventListener("click", function () {
+  let randomEmoji1 = Math.floor(Math.random() * fighters.length);
+  let randomEmoji2 = Math.floor(Math.random() * fighters.length);
+  stageEl.textContent = fighters[randomEmoji1] + " vs " + fighters[randomEmoji2]
+});
+
